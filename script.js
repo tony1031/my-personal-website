03c5809dc0d2ae8e40da7f1a11c330a029f12014
jose <tonyjimenez099.tj@gmail.com>
2022-06-19 21:25:38 -0500

    add script.js, to index.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let addToDoButton = document.getElementById('addToDo');
 let ToDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField')
+let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
