4c0c7310ca447fc338fb7e24b0c329b23e008378
jose <tonyjimenez099.tj@gmail.com>
2022-06-19 21:12:40 -0500

    add script.js, to index.html

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ let ToDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField')
 
 addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
     ToDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        paragraph.style.removeChild(paragraph);
+    })
 })
